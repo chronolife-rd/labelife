78ae62b1cfc2b10c342b973807f74177c24d99ea
BlandineL <109736806+BlandineL@users.noreply.github.com>
2022-12-06 12:16:23 +0100

    update label rules in text box

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
 # Constantes
-QUALITY_OPTIONS = ('-', 'Excellent : no noise', 'Good : noise on P', 'Medium : medium noise on T and R', 'Bad : just R peaks', 'Worst')
+QUALITY_OPTIONS = ('-', 'Excellent : no noise, PQRST visible on wholde seg', 'Good : QT length ok on whole seg', 'Medium : R wave ok on whole seg', 'Bad : Some missing Rs or mistake prone signal', 'Worst : mostly noise')
 QUALITY_DICT    = {'-':0, 'Excellent':1, 'Good':2, 'Medium':3, 'Bad': 4, 'Worst':5}
 
 PATH_IMAGE       = 'image/'
